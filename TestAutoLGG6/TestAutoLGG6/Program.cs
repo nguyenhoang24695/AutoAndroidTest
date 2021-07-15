a480d7064992d04dc8b7a4fb55f780bc0b050622
Your Name <you@example.com>
2021-07-15 21:38:52 +0700

    init

---
+++ TestAutoLGG6/TestAutoLGG6/Program.cs
@@ -0,0 +1,15 @@
+﻿using KAutoHelper;
+using System;
+
+namespace TestAutoLGG6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ADBHelper.SetADBFolderPath(Environment.CurrentDirectory);
+            var devices = ADBHelper.GetDevices();
+
+        }
+    }
+}
